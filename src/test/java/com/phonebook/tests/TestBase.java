31017302f351314459d61c9c002ba3455ca1cdb7
MariiaRoglev <Mariarogaleva06@gmail.com>
2025-04-07 20:57:02 +0300

    CW_07.04

--- src/test/java/com/phonebook/tests/TestBase.java
+++ src/test/java/com/phonebook/tests/TestBase.java
@@ -4,11 +4,13 @@ import com.phonebook.fw.ApplicationManager;
 import org.openqa.selenium.remote.Browser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
+import java.io.IOException;
 import java.lang.reflect.Method;
 
 public class TestBase
@@ -36,10 +38,22 @@ public class TestBase
     }
 
     @AfterMethod
-    public void stopTest()
+    public void stopTest(ITestResult result) throws IOException//for screenshot
     {
+        if (result.isSuccess())
+        {
+            logger.info("PASSED: "+result.getMethod().getMethodName());
+        }
+        else
+        {
+            logger.error("FAILED: "+result.getMethod().getMethodName()+"Screenshot path:" +app.getUser().takeScreenshot());
+        }
         logger.info("Stop test");
+        logger.info("=============================================");
     }
+
+
+
 
 
 
