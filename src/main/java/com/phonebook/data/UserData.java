7f630c0fc0038479a65085ff1a9ae099416e2b47
MariiaRoglev <Mariarogaleva06@gmail.com>
2025-04-07 20:59:17 +0300

    CW_07.04

--- src/main/java/com/phonebook/data/UserData.java
+++ src/main/java/com/phonebook/data/UserData.java
@@ -1,4 +1,13 @@
 package com.phonebook.data;
 
-public class UserData {
+public class UserData
+{
+    public static final String EMAIL = "kashamasha@gmail.com";
+    public static final String PASSWORD = "QwertyQwerty1!";
+
+
+
+
+
+
 }
